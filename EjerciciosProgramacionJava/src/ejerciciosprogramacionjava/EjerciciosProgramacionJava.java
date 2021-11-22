cfbe62dee6749bb14c60b43a04f98d8a5575109b
Ian Molina <ianmg.daw@gmail.com>
2021-11-22 19:49:50 +0100

    Creado proyecto Java

---
+++ EjerciciosProgramacionJava/src/ejerciciosprogramacionjava/EjerciciosProgramacionJava.java
@@ -0,0 +1,21 @@
+/*
+ * Proyecto EjerciciosProgramacionJava - Archivo EjerciciosProgramacionJava.java - Compañia DAW
+ * Licencia Creative Commons BY-NC-SA 4.0
+ * https://creativecommons.org/licenses/by-nc-sa/4.0/
+ */
+package ejerciciosprogramacionjava;
+
+/**
+ *
+ * @author Ian Molina Guillo <ianmg.daw@gmail.com>
+ */
+public class EjerciciosProgramacionJava {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
