e4415756718080983a99f074ad2f872cbab6c79c
Ian Molina <ianmg.daw@gmail.com>
2021-11-23 21:07:38 +0100

    Hacer el ejercicio Caso Practico 0 v1

---
+++ EjerciciosProgramacionJava/src/ud5EjerciciosFunciones/Scanner.java
@@ -0,0 +1,16 @@
+/*
+ * Proyecto EjerciciosProgramacionJava - Archivo Scanner.java - Companía DAW
+ * Licencia Creative Commons BY-NC-SA 4.0
+ * https://creativecommons.org/licenses/by-nc-sa/4.0/
+ */
+package ud5EjerciciosFunciones;
+
+/**
+ *
+ * @author Ian Molina Guilló <ianmg.daw@gmail.com>
+ * @version 1.0
+ * @date 23 nov. 2021 13:37:26
+ */
+class Scanner {
+
+}
