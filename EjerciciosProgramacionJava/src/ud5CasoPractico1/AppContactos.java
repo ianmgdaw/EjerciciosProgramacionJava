84377ae719171d42e566544d65023e48e430a5af
Ian Molina <ianmg.daw@gmail.com>
2021-11-25 16:20:46 +0100

    Creado paquete ud5CasoPractico1

---
+++ EjerciciosProgramacionJava/src/ud5CasoPractico1/AppContactos.java
@@ -0,0 +1,18 @@
+/*
+ * Proyecto EjerciciosProgramacionJava - Archivo AppContactos.java - Companía DAW
+ * Licencia Creative Commons BY-NC-SA 4.0
+ * https://creativecommons.org/licenses/by-nc-sa/4.0/
+ */
+package ud5CasoPractico1;
+
+/**
+ *
+ * @author Ian Molina Guilló <ianmg.daw@gmail.com>
+ * @version 1.0
+ * @date 25 nov. 2021 11:48:57
+ */
+public class AppContactos {
+    
+    // Muestra el menú, pide al usuario valor rango 1-8 y devuelve la opción elegida. ============================
+    
+}
