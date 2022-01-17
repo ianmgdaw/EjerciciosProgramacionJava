6f2ded4307fa2b85acb3950c000c6736a6908517
Ian Molina <ianmg.daw@gmail.com>
2022-01-17 20:55:25 +0100

    UD 6 Ejercicios2 v1.19.1315

--- EjerciciosProgramacionJava/src/UD6/Ejercicios2/Ejer4ClaseReloj/MainReloj.java
+++ EjerciciosProgramacionJava/src/UD6/Ejercicios2/Ejer4ClaseReloj/MainReloj.java
@@ -14,6 +14,43 @@ package UD6.Ejercicios2.Ejer4ClaseReloj;
 public class MainReloj {
 
     public static void main(String[] args) {
-        
+
+        Reloj r0 = new Reloj(0, 0, 0);
+
+        System.out.println("Reloj 0");
+        r0.imprimeHora();
+        r0.imprimeHora12();
+        System.out.println("");
+
+        Reloj r1 = new Reloj(10, 30, 15);
+
+        System.out.println("Reloj 1");
+        r1.imprimeHora();
+        r1.imprimeHora12();
+        r1.tick();
+        r1.imprimeHora();
+        r1.imprimeHora12();
+        System.out.println("");
+
+        Reloj r2 = new Reloj(10, 59, 59);
+
+        System.out.println("Reloj 2");
+        r2.imprimeHora();
+        r2.imprimeHora12();
+        r2.tick();
+        r2.imprimeHora();
+        r2.imprimeHora12();
+        System.out.println("");
+
+        Reloj r3 = new Reloj(23, 59, 59);
+
+        System.out.println("Reloj 3");
+        r3.imprimeHora();
+        r3.imprimeHora12();
+        r3.tick();
+        r3.imprimeHora();
+        r3.imprimeHora12();
+        System.out.println("");
+
     }
 }
